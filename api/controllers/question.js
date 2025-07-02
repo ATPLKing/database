b6699870bef2da751342e7b484e64de6232ef908
Chesterkxng <cgoita00@gmail.com>
2025-07-02 15:37:20 +0200

    docs: questionController documented

--- api/controllers/question.js
+++ api/controllers/question.js
@@ -1,6 +1,17 @@
 const path = require("path");
 const fs = require("fs");
 
+/**
+ * Handles the request to retrieve questions based on the specified UV parameter.
+ * 
+ * @param {Object} req - The request object containing parameters.
+ * @param {Object} res - The response object used to send back the result.
+ * 
+ * Checks if the questions.json file exists in the specified UV directory.
+ * If the file exists, reads and parses the JSON data, then sends it as a response.
+ * If the file does not exist, sends a 404 error response.
+ * If an error occurs during file reading, sends a 500 error response.
+ */
 exports.getQuestionsbyUV = (req, res) => {
   const uv = req.params.uv;
   const filePath = path.join(__dirname, "../../json", uv, "questions.json");
